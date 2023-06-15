8402e1d56c023d1236b5fc3a07cc9347ffbde125
Atharva Pardeshi <atharva.exe@gmail.com>
2023-06-15 10:56:25 +0530

    refactor: move info card to its own component file

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,11 @@
 "use client"
 
-
-import Image from "next/image"
 import { useState } from "react"
-
-import Art from "@/public/weird-rect-thing.gif"
-
-import { Laptop, Github } from "lucide-react"
 
 import { DONATION_IN_CENTS, MAX_DONATION } from "@/config"
 
 import Profile from "@/components/profile"
-
-import { Card, CardContent } from "@/components/ui/card"
-import { AspectRatio } from "@/components/ui/aspect-ratio"
-
-import Link from "next/link"
+import InfoCard from "@/components/info-card"
 
 export default function Home() {
   const [error, setError] = useState(null)
@@ -31,26 +21,7 @@ export default function Home() {
       <Profile />
 
       <div className="flex flex-col md:flex-row justify-center items-center">
-        <Card className="w-3/5">
-          <CardContent>
-            <AspectRatio ratio={16 / 9}>
-              <Image src={Art} alt="Generative art thing" fill />
-            </AspectRatio>
-
-            <p className="leading-normal text-muted-foreground mt-4 p-4">
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages.
-            </p>
-
-            <div className="flex flex-row gap-4 p-4 pb-0">
-              <Link href="https://github.com/SazedWorldbringer">
-                <Github className="text-muted-foreground hover:text-foreground hover:scale-105 transition duration-200" />
-              </Link>
-              <Link href="https://atharvapardeshi.vercel.app">
-                <Laptop className="text-muted-foreground hover:text-foreground hover:scale-105 transition duration-200" />
-              </Link>
-            </div>
-          </CardContent>
-        </Card>
+        <InfoCard />
 
         <div className="w-2/5">
           <h1>Buy me a book</h1>
---
+++ components/info-card.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image"
+import Link from "next/link"
+
+import Art from "@/public/weird-rect-thing.gif"
+
+import { AspectRatio } from "./ui/aspect-ratio"
+import { Card, CardContent } from "./ui/card"
+
+import { Github, Laptop } from "lucide-react"
+
+const InfoCard = () => {
+	return (
+		<Card className="w-3/5">
+			<CardContent>
+				<AspectRatio ratio={16 / 9}>
+					<Image src={Art} alt="Generative art thing" fill />
+				</AspectRatio>
+
+				<p className="leading-normal text-muted-foreground mt-4 p-4">
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages.
+				</p>
+
+				<div className="flex flex-row gap-4 p-4 pb-0">
+					<Link href="https://github.com/SazedWorldbringer">
+						<Github className="text-muted-foreground hover:text-foreground hover:scale-105 transition duration-200" />
+					</Link>
+					<Link href="https://atharvapardeshi.vercel.app">
+						<Laptop className="text-muted-foreground hover:text-foreground hover:scale-105 transition duration-200" />
+					</Link>
+				</div>
+			</CardContent>
+		</Card>
+	)
+}
+
+export default InfoCard
