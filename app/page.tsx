423c897dceff0472336258656e1893faf69d78df
Atharva Pardeshi <atharva.exe@gmail.com>
2024-01-16 22:16:30 +0530

    refactor: change height and margin

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,10 @@ import DonationCard from "@/components/donation-card"
 export default function Home() {
 
   return (
-    <main className="container max-w-6xl h-screen flex flex-col justify-center items-center m-auto">
+    <main className="container max-w-6xl h-full flex flex-col justify-center items-center m-auto">
       <Profile />
 
-      <div className="flex flex-col md:flex-row justify-center items-start gap-8">
+      <div className="flex flex-col md:flex-row justify-center items-start gap-8 mb-6">
         <InfoCard />
         <DonationCard />
       </div>
--- components/info-card.tsx
+++ components/info-card.tsx
@@ -15,7 +15,7 @@ const InfoCard = () => {
 					<Image src={Art} alt="Generative art thing" fill />
 				</AspectRatio>
 
-				<p className="leading-normal text-muted-foreground mt-4 p-4">
+				<p className="leading-normal text-muted-foreground mt-[-1.4rem] p-4">
 					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages.
 				</p>
 
