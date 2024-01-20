98bf32475d4edf4c873bb6fd12999f8965ee45ff
Atharva Pardeshi <atharva.exe@gmail.com>
2024-01-20 20:06:38 +0530

    feat: add keys from env

--- config.ts
+++ config.ts
@@ -11,3 +11,7 @@ export const MAX_DONATION = parseInt(
 export const STRIPE_API_KEY = process.env.STRIPE_API_KEY as string
 
 export const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET as string
+
+export const MERCHANT_ID = process.env.PHONEPE_MERCHANT_ID
+export const SALT_KEY = process.env.PHONEPE_SALT_KEY
+export const SALT_INDEX = process.env.PHONEPE_SALT_INDEX
